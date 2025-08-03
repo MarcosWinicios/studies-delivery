84355afa29c4e4f0106bc4197701d5d28b1dff3b
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-08-03 20:10:48 -0300

    feat(courier): Add domain model classes -> Courier and AssignedDelivery

---
+++ Microservices/Courier-Management/src/main/java/com/studies/studiesdelivery/courier/management/domain/model/AssignedDelivery.java
@@ -0,0 +1,30 @@
+package com.studies.studiesdelivery.courier.management.domain.model;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+public class AssignedDelivery {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private OffsetDateTime assignedAt;
+
+    static AssignedDelivery brandNew(UUID deliveryId) {
+        AssignedDelivery assignedDelivery = new AssignedDelivery();
+        assignedDelivery.setId(deliveryId);
+        assignedDelivery.setAssignedAt(OffsetDateTime.now());
+        return assignedDelivery;
+    }
+
+}
---
+++ Microservices/Courier-Management/src/main/java/com/studies/studiesdelivery/courier/management/domain/model/Courier.java
@@ -0,0 +1,53 @@
+package com.studies.studiesdelivery.courier.management.domain.model;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor(access = lombok.AccessLevel.PACKAGE)
+@Setter(AccessLevel.PRIVATE)
+public class Courier {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Setter(AccessLevel.PUBLIC)
+    private String name;
+
+    @Setter(AccessLevel.PUBLIC)
+    private String phone;
+
+    private Integer fulfilledDeliveriesQuantity;
+
+    private Integer pendingDeliveriesQuantity;
+
+    private OffsetDateTime lastFulFilledDeliveryAt;
+
+    private List<AssignedDelivery> pendingDeliveries = new ArrayList<>();
+
+    public List<AssignedDelivery> getPendingDeliveries() {
+        return Collections.unmodifiableList(this.pendingDeliveries);
+    }
+
+    public static Courier brandNew(String name, String phone){
+        Courier courier = new Courier();
+
+        courier.setId(UUID.randomUUID());
+        courier.setName(name);
+        courier.setPhone(phone);
+        courier.setFulfilledDeliveriesQuantity(0);
+        courier.setPendingDeliveriesQuantity(0);
+
+        return courier;
+    }
+}
