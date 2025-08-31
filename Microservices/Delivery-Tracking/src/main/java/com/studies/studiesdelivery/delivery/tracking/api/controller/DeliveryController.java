759158aec4c644e5d41349a4da1fe3e845d87984
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-08-31 15:23:19 -0300

    feat(delivery-tracking): Add search endpoints

--- Microservices/Delivery-Tracking/src/main/java/com/studies/studiesdelivery/delivery/tracking/api/controller/DeliveryController.java
+++ Microservices/Delivery-Tracking/src/main/java/com/studies/studiesdelivery/delivery/tracking/api/controller/DeliveryController.java
@@ -2,10 +2,15 @@ package com.studies.studiesdelivery.delivery.tracking.api.controller;
 
 import com.studies.studiesdelivery.delivery.tracking.api.model.DeliveryInput;
 import com.studies.studiesdelivery.delivery.tracking.domain.model.Delivery;
+import com.studies.studiesdelivery.delivery.tracking.domain.repository.DeliveryRepository;
 import com.studies.studiesdelivery.delivery.tracking.domain.service.DeliveryPreparationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -22,16 +28,27 @@ import java.util.UUID;
 public class DeliveryController {
 
     private final DeliveryPreparationService deliveryPreparationService;
+    private final DeliveryRepository deliveryRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Delivery draft(@RequestBody @Valid DeliveryInput deliveryInput){
-        return  deliveryPreparationService.draft(deliveryInput);
+    public Delivery draft(@RequestBody @Valid DeliveryInput deliveryInput) {
+        return deliveryPreparationService.draft(deliveryInput);
     }
 
     @PutMapping("/{deliveryId}")
-    public Delivery edit(@PathVariable UUID deliveryId, @RequestBody @Valid DeliveryInput deliveryInput){
-        return  deliveryPreparationService.edit(deliveryId, deliveryInput);
+    public Delivery edit(@PathVariable UUID deliveryId, @RequestBody @Valid DeliveryInput deliveryInput) {
+        return deliveryPreparationService.edit(deliveryId, deliveryInput);
     }
 
+    @GetMapping
+    public PagedModel<Delivery> findAll(@PageableDefault Pageable pageable) {
+        return new PagedModel<>(deliveryRepository.findAll(pageable));
+    }
+
+    @GetMapping("/{deliveryId}")
+    public Delivery findById(@PathVariable UUID deliveryId){
+        return deliveryRepository.findById(deliveryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 }
