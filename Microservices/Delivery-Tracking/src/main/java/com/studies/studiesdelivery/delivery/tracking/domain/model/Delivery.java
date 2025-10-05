c6c992559ba2df590fb5534079bcf4bf3c2e3c2c
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-10-04 22:47:54 -0300

    feat(delivery-tracking): Add domain events

--- Microservices/Delivery-Tracking/src/main/java/com/studies/studiesdelivery/delivery/tracking/domain/model/Delivery.java
+++ Microservices/Delivery-Tracking/src/main/java/com/studies/studiesdelivery/delivery/tracking/domain/model/Delivery.java
@@ -1,5 +1,8 @@
 package com.studies.studiesdelivery.delivery.tracking.domain.model;
 
+import com.studies.studiesdelivery.delivery.tracking.domain.event.DeliveryFulfilledEvent;
+import com.studies.studiesdelivery.delivery.tracking.domain.event.DeliveryPickUpEvent;
+import com.studies.studiesdelivery.delivery.tracking.domain.event.DeliveryPlacedEvent;
 import com.studies.studiesdelivery.delivery.tracking.domain.exception.DomainException;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.AttributeOverrides;
@@ -16,6 +19,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.domain.AbstractAggregateRoot;
 
 import java.math.BigDecimal;
 import java.time.Duration;
@@ -27,17 +31,17 @@ import java.util.UUID;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 @Setter(AccessLevel.PRIVATE)
 @Getter
-public class Delivery {
+public class Delivery extends AbstractAggregateRoot<Delivery> {
 
     @Id
     @EqualsAndHashCode.Include
     private UUID id;
     private UUID courierId;
 
-    private  DeliveryStatus status;
+    private DeliveryStatus status;
 
     private OffsetDateTime placedAt;
     private OffsetDateTime assignedAt;
@@ -52,30 +56,30 @@ public class Delivery {
 
     @Embedded
     @AttributeOverrides({
-            @AttributeOverride(name =  "zipCode", column = @Column(name = "sender_zip_code")),
-            @AttributeOverride(name =  "street", column = @Column(name = "sender_street")),
-            @AttributeOverride(name =  "number", column = @Column(name = "sender_number")),
-            @AttributeOverride(name =  "complement", column = @Column(name = "sender_complement")),
-            @AttributeOverride(name =  "name", column = @Column(name = "sender_name")),
-            @AttributeOverride(name =  "phone", column = @Column(name = "sender_phone"))
+            @AttributeOverride(name = "zipCode", column = @Column(name = "sender_zip_code")),
+            @AttributeOverride(name = "street", column = @Column(name = "sender_street")),
+            @AttributeOverride(name = "number", column = @Column(name = "sender_number")),
+            @AttributeOverride(name = "complement", column = @Column(name = "sender_complement")),
+            @AttributeOverride(name = "name", column = @Column(name = "sender_name")),
+            @AttributeOverride(name = "phone", column = @Column(name = "sender_phone"))
     })
     private ContactPoint sender;
 
     @Embedded
     @AttributeOverrides({
-            @AttributeOverride(name =  "zipCode", column = @Column(name = "recipient_zip_code")),
-            @AttributeOverride(name =  "street", column = @Column(name = "recipient_street")),
-            @AttributeOverride(name =  "number", column = @Column(name = "recipient_number")),
-            @AttributeOverride(name =  "complement", column = @Column(name = "recipient_complement")),
-            @AttributeOverride(name =  "name", column = @Column(name = "recipient_name")),
-            @AttributeOverride(name =  "phone", column = @Column(name = "recipient_phone"))
+            @AttributeOverride(name = "zipCode", column = @Column(name = "recipient_zip_code")),
+            @AttributeOverride(name = "street", column = @Column(name = "recipient_street")),
+            @AttributeOverride(name = "number", column = @Column(name = "recipient_number")),
+            @AttributeOverride(name = "complement", column = @Column(name = "recipient_complement")),
+            @AttributeOverride(name = "name", column = @Column(name = "recipient_name")),
+            @AttributeOverride(name = "phone", column = @Column(name = "recipient_phone"))
     })
     private ContactPoint recipient;
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "delivery")
-    private List<Item> items =  new ArrayList<>();
+    private List<Item> items = new ArrayList<>();
 
-    public static Delivery draft(){
+    public static Delivery draft() {
         Delivery delivery = new Delivery();
 
         delivery.setId(UUID.randomUUID());
@@ -85,10 +89,10 @@ public class Delivery {
         delivery.setCourierPayout(BigDecimal.ZERO);
         delivery.setDistanceFee(BigDecimal.ZERO);
 
-        return  delivery;
+        return delivery;
     }
 
-    public UUID addItem(String name, int quantity){
+    public UUID addItem(String name, int quantity) {
         Item item = Item.brandNew(name, quantity, this);
         this.items.add(item);
 
@@ -97,12 +101,12 @@ public class Delivery {
         return item.getId();
     }
 
-    public void removeItem(UUID itemId){
+    public void removeItem(UUID itemId) {
         this.items.removeIf(item -> item.getId().equals(itemId));
         this.calculateTotalItems();
     }
 
-    public void changeItemQuantity(UUID itemId, int quantity){
+    public void changeItemQuantity(UUID itemId, int quantity) {
         Item item = getItems().stream().filter(i -> i.getId().equals(itemId))
                 .findFirst().orElseThrow();
 
@@ -110,12 +114,12 @@ public class Delivery {
         calculateTotalItems();
     }
 
-    public void removeItems(){
+    public void removeItems() {
         this.items.clear();
         this.calculateTotalItems();
     }
 
-    public void editPreparationDetails(PreparationDetails details){
+    public void editPreparationDetails(PreparationDetails details) {
 
         verifyIfCanBeEdited();
 
@@ -128,21 +132,28 @@ public class Delivery {
 
     }
 
-    public void place (){
+    public void place() {
         verifyIfCanBePlaced();
 
         this.changeStatusTo(DeliveryStatus.WAITING_FOR_COURIER);
         this.setPlacedAt(OffsetDateTime.now());
+
+        super.registerEvent(new DeliveryPlacedEvent(this.getPlacedAt(), this.getId()));
     }
 
-    public void pickUp(UUID courierId){
+    public void pickUp(UUID courierId) {
         this.setCourierId(courierId);
         this.changeStatusTo(DeliveryStatus.IN_TRANSIT);
         this.setAssignedAt(OffsetDateTime.now());
+
+        super.registerEvent(new DeliveryPickUpEvent(this.getAssignedAt(), this.getId()));
     }
-    public void markAsDelivered(){
+
+    public void markAsDelivered() {
         this.changeStatusTo(DeliveryStatus.DELIVERED);
         this.setFulfilledAt(OffsetDateTime.now());
+
+        super.registerEvent(new DeliveryFulfilledEvent(this.getFulfilledAt(), this.getId()));
     }
 
 
@@ -150,7 +161,7 @@ public class Delivery {
         return Collections.unmodifiableList(this.items);
     }
 
-    private void calculateTotalItems(){
+    private void calculateTotalItems() {
         int totalItems = getItems().stream().
                 mapToInt(Item::getQuantity)
                 .sum();
@@ -159,11 +170,11 @@ public class Delivery {
     }
 
     private void verifyIfCanBePlaced() {
-        if(!isFilled()){
+        if (!isFilled()) {
             throw new DomainException();
         }
 
-        if(!getStatus().equals(DeliveryStatus.DRAFT)){
+        if (!getStatus().equals(DeliveryStatus.DRAFT)) {
             throw new DomainException();
         }
     }
@@ -175,13 +186,13 @@ public class Delivery {
     }
 
     private void verifyIfCanBeEdited() {
-        if(!getStatus().equals(DeliveryStatus.DRAFT)){
+        if (!getStatus().equals(DeliveryStatus.DRAFT)) {
             throw new DomainException();
         }
     }
 
     private void changeStatusTo(DeliveryStatus newStatus) {
-        if(newStatus != null && this.getStatus().canNotChangeTo(newStatus)){
+        if (newStatus != null && this.getStatus().canNotChangeTo(newStatus)) {
             throw new DomainException("Invalid status transition from " + getStatus() + " to " + newStatus);
         }
         this.setStatus(newStatus);
